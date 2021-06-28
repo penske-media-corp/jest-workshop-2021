d928d4d26f10982555f2c83cc0f22429cd0c588b
Mike Griego <mgriego@pmc.com>
2021-06-28 14:55:58 -0500

    Fix typos.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,11 @@
 import { add } from '../index';
 
 describe('add', () => {
-    test('returns the correct answer', () => {
+    test('Returns the correct answer', () => {
         expect(add(1, 2)).toBe(3);
     });
 
-    test('throws when non-number past', () => {
+    test('Throws when non-number passed', () => {
         expect(() => {
             add('1', 3);
         }).toThrow('Parameter was not a number.');
