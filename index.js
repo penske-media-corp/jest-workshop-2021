1e893cf410b65bd435e892fdf22dabad3404effc
Michael Auteri <mauteri@gmail.com>
2021-07-16 18:00:20 -0400

    Convert back to spaces.

--- index.js
+++ index.js
@@ -7,16 +7,16 @@
  * @throws {Error}
  */
 export function add(first, second) {
-	if (typeof first !== 'number' || typeof second !== 'number') {
-		throw new Error('Parameter was not a number.');
-	}
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        throw new Error('Parameter was not a number.');
+    }
 
-	return first + second;
+    return first + second;
 }
 
 export function addListItem($el, text) {
-	const li = document.createElement('li');
+    const li = document.createElement('li');
 
-	li.appendChild(document.createTextNode(text));
-	$el.appendChild(li);
+    li.appendChild(document.createTextNode(text));
+    $el.appendChild(li);
 }
