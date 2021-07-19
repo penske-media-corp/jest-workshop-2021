10a494b338f980a17e0a95b68d4d39b47387ca6b
David Parsons <djp424@gmail.com>
2021-07-19 13:54:54 -0600

    cleanup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ export function add(first, second) {
     return first + second;
 }
 
-/*
+/**
  * Add a new item to a list.
  *
  * @param {HTMLOListElement|HTMLUListElement} $el Unordered or order list element
@@ -29,7 +29,7 @@ export function addListItem($el, text) {
     $el.appendChild(li);
 }
 
-/*
+/**
  * Is a number positive.
  *
  * @param {number} number
